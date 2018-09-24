5598937223df4e804538599c74901652237ef43c
m-yasuno <43286810+m-yasuno@users.noreply.github.com>
2018-09-24 12:31:32 +0900

    simple

--- yasuno/ line_graph.py
+++ yasuno/ line_graph.py
@@ -1,72 +1,36 @@
 
 # coding: utf-8
 
-# In[1]:
-
-
-#モジュールインポート
+#import module
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-plt.style.use('ggplot')
-from datetime import datetime
 from PIL import Image
 
-#テスト用(CPU取得)
-import psutil
+#variable
+x = [1,2,3,4,5,6,7,8,9,10]
+y1 = [1,2,3,4,5,6,7,8,9,10] #センサの値を入れる予定
 
+# figure
+fig = plt.figure(figsize=(2.4,2.2)) #サイズ設定
+fig.subplots_adjust(bottom=0.16, left=0.22) #余白設定
 
-# In[2]:
+# graph
+ax = fig.add_subplot(1,1,1) #グラフ作成
+ax.set_xlabel("Time") #xラベル
+ax.set_ylabel("Angerlevel") #yラベル
+ax.set_title('TimeSeriesGraph') #グラフタイトル
+ax.xaxis.set_major_formatter(plt.NullFormatter()) #x軸の値を非表示
+ax.plot(x, y1)
 
-
-#変数セット
-x = []
-y1 = []
-i = 0
-
-
-# In[3]:
-
-
-#while True:
-while i < 30:
-    nowtime = datetime.now()
-    time = nowtime.strftime("%H:%M:%S")
-    x.append(time) #現在の時刻取得
-    
-    cpu_percent = psutil.cpu_percent(interval=1) #CPU取得(テスト用)
-    y1.append(cpu_percent)
-    #y2.append(i)
-    i += 1
-    
-    if i % 10 == 0:
-        # figure
-        fig, ax = plt.subplots(figsize=(2.4,2.4))
-        ax.xaxis.set_major_formatter(plt.NullFormatter())
+# save as png
+plt.savefig('line_graph.png')
         
-        # plot
-        ax.plot(x, y1, linestyle='-', color='b', label='ang')
+# Convert jpeg
+im = Image.open('line_graph.png')
+rgb_im = im.convert('RGB')
+rgb_im.save('line_graph.jpg','JPEG')
         
-        # x axis
-        #plt.xlim([0 , 255]) #グラフの範囲
-        ax.set_xlabel('日時')
-
-        # y axis
-        #ax.set_ylabel('y')
-        
-        # legend and title
-        ax.legend(loc='best')
-        ax.set_title('title')
-
-        # save as png
-        plt.savefig('figure_{0}.png'.format(i))
-        
-        # Convert jpeg
-        im = Image.open('figure_{0}.png'.format(i))
-        rgb_im = im.convert('RGB')
-        rgb_im.save('figure_{0}.jpg'.format(i),'JPEG')
-        
-plt.close('all')
+plt.close()
 
